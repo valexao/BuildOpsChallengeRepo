dfbe7b0ac4a326e6c6fb341b56fd6be4ed980393
Valexa Orelien <vorelien@stanford.edu>
2020-06-01 23:04:41 -0400

    First Commit

---
+++ Box.js
@@ -0,0 +1,21 @@
+import { ReactComponent } from "*.svg";
+
+class Box extends React.Component {
+    render() {
+        var contentStyle = {
+            paddingTop: "30px",
+            margin:"10px",
+            display:"table",
+            height: "1000",
+            width: "500",
+            backgroundColor: "#FF6663"
+          };
+
+        return (
+            <div style = {contentStyle}>
+                
+            </div>
+        )
+    }
+
+}
--- src/App.js
+++ src/App.js
@@ -1,26 +1,87 @@
 import React from 'react';
-import logo from './logo.svg';
+import data from './data.json';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {isDefault: true, buttonString: "-", data: data};
+
+    //binding to make the callback work
+    this.clicked = this.clicked.bind(this);
+  }
+
+  //Toggle between 
+  clicked() {
+    this.setState((state) => ({ isDefault: !state.isDefault}));
+    // if (this.state.buttonString == "-") {
+    //   this.state.buttonString = this.setState((state) => ({buttonString: "+"}));
+    // } else {
+    //   this.state.buttonString = this.setState((state) => ({buttonString: "-"}));
+    // }
+  };
+
+  //Extract information from headerData and render it horizontally
+  renderHeaderData = () => {
+    var headerData = this.state.data.headerData.slice();
+    
+    var objects = headerData.map(function(item, index) {
+      return <div style={{display:"inline", padding:"50px"}} key={index}>{item}</div>
+    });
+    return objects;
+  };
+
+  changeColor = () => {
+    var contentA = this.state.data.contentA;
+    var contentB = this.state.data.contentB;
+    //CONTENT A Default #FFFFFF (VISIBLE)
+
+    //CONTENT B Default #EEEEEE (INVISIBLE)
+  };
+
+  //Load the content for panel A
+  loadContentA() {
+    var contentA = this.state.data.contentA;
+
+    if(this.state.isDefault) {
+      return <div style={{background:"#FFFFFF", float:"left"}}>{contentA}</div>
+    }
+  }
+
+  //Load the content for panel B
+  loadContentB() {
+    var contentB = this.state.data.contentB;
+    if(!this.state.isDefault) {
+      return <div style={{background:"#EEEEEE", float:"right"}}>{contentB}</div>
+    }
+  }
+
+  render() {
+    var contentStyle = {
+      paddingTop: "30px",
+      margin:"10px",
+      display:"table",
+      height: "1000",
+      width: "500",
+      filter: "drop-shadow(0px 0px 2px #600)"
+    };
+
+    return(
+      <div style={{color: 'black', margin:"30px", width:"100%"}}>
+        {this.renderHeaderData()}
+        <button type='button' onClick={this.clicked}>{this.state.isDefault ? '-' : '+'}</button>
+        <div style={contentStyle}>
+        {this.loadContentA()}
+        {this.loadContentB()}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
+
+//ReactDOM.render(<App />, document.getElementById('app'));
+//           {this.loadContentA()}
+          
+//{this.loadContentB()}
